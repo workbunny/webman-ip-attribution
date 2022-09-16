728c155cedb7acb7f07f9d5e76435930b0818dfe
chaz6chez <250220719@qq.com>
2022-09-16 16:19:37 +0800

    updated: 1. 完善测试用例

---
+++ tests/LocationTest.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use Workbunny\WebmanIpAttribution\Exceptions\IpAttributionException;
+use Workbunny\WebmanIpAttribution\Location;
+
+class LocationTest extends AbstractTest
+{
+
+    public function testGetLocationByV4()
+    {
+        $res = (new Location())->getLocation('8.8.8.8');
+
+        $this->assertEquals([
+            'country' => 'United States',
+            'city' => '--',
+            'asn' => 'GOOGLE',
+            'continent' => 'North America',
+            'timezone' => 'America/Chicago',
+        ], $res);
+    }
+
+    public function testGetLocationByV6()
+    {
+        $res = (new Location())->getLocation('::0808:0808');
+
+        $this->assertEquals([
+            'country' => 'United States',
+            'city' => '--',
+            'asn' => 'GOOGLE',
+            'continent' => 'North America',
+            'timezone' => 'America/Chicago',
+        ], $res);
+    }
+
+    public function testGetLocationLocalIpV4()
+    {
+        $this->expectException(IpAttributionException::class);
+        $this->expectExceptionMessage('Ip Not Found.');
+        $this->expectExceptionCode(0);
+
+        $res = (new Location())->getLocation('127.0.0.1');
+        $this->assertEquals([], $res);
+    }
+
+    public function testGetLocationLocalIpV6()
+    {
+        $this->expectException(IpAttributionException::class);
+        $this->expectExceptionMessage('Ip Not Found.');
+        $this->expectExceptionCode(0);
+
+        $res = (new Location())->getLocation('::7f00:0001');
+        $this->assertEquals([], $res);
+    }
+
+    public function testGetLocationPrivateIpV4()
+    {
+        $this->expectException(IpAttributionException::class);
+        $this->expectExceptionMessage('Ip Not Found.');
+        $this->expectExceptionCode(0);
+
+        $res = (new Location())->getLocation('192.168.0.1');
+        $this->assertEquals([], $res);
+    }
+
+    public function testGetLocationPrivateIpV6()
+    {
+        $this->expectException(IpAttributionException::class);
+        $this->expectExceptionMessage('Ip Not Found.');
+        $this->expectExceptionCode(0);
+
+        $res = (new Location())->getLocation('::c0a8:0001');
+        $this->assertEquals([], $res);
+    }
+}
--- tests/ReaderTest.php
+++ tests/ReaderTest.php
@@ -11,6 +11,10 @@ use Workbunny\WebmanIpAttribution\Location;
 class ReaderTest extends AbstractTest
 {
 
+    /**
+     * 普通的创建reader测试
+     * @return void
+     */
     public function testCreateReader()
     {
         $location = new Location();
@@ -22,6 +26,10 @@ class ReaderTest extends AbstractTest
         $this->assertCount(3, $location->getReaders());
     }
 
+    /**
+     * 创建reader的单例测试
+     * @return void
+     */
     public function testCreateReaderInstance()
     {
         $location = new Location();
@@ -41,12 +49,56 @@ class ReaderTest extends AbstractTest
         $this->assertCount(1, $location->getReaders());
     }
 
+    /**
+     * 使用非法参数创建reader的测试
+     * @return void
+     */
     public function testCreateReaderInvalidArgument()
     {
         $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('invalid db.');
+        $this->expectExceptionCode(1);
 
         $location = new Location();
         $location->createReader('abc');
         $this->assertCount(0, $location->getReaders());
+    }
+
+    /**
+     * 移除所有reader的测试
+     * @return void
+     */
+    public function testRemoveReader()
+    {
+        $location = new Location();
+        $location->createReader(Location::DB_COUNTRY);
+        $location->createReader(Location::DB_CITY);
+        $location->createReader(Location::DB_ASN);
+        $this->assertCount(3, $location->getReaders());
+
+        $location->removeReader();
+        $this->assertCount(0, $location->getReaders());
+    }
+
+    /**
+     * 移除单个reader的测试
+     * @return void
+     */
+    public function testRemoveReaderBySingle()
+    {
+        $location = new Location();
+        $location->createReader(Location::DB_COUNTRY);
+        $location->createReader(Location::DB_CITY);
+        $location->createReader(Location::DB_ASN);
+        $this->assertCount(3, $location->getReaders());
+
+        $location->removeReader(Location::DB_ASN);
+        $this->assertCount(2, $location->getReaders());
+
+        $location->removeReader(Location::DB_ASN);
+        $this->assertCount(2, $location->getReaders());
+
+        $location->removeReader(Location::DB_COUNTRY);
+        $this->assertCount(1, $location->getReaders());
     }
 }
